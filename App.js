2172e62ecda5de749c1ef40518a69a0334782fe2
Anna Tasheva <anna.tasheva90@gmail.com>
2024-08-12 14:11:32 +0100

    Completed all React Extension Tasks

--- App.js
+++ App.js
@@ -1,21 +1,20 @@
-import React from 'react';
+import React from "react";
 // Import data and WeatherCard here
-
+const cities = require("./data");
+import WeatherCard from "./components/WeatherCard";
 
 function App() {
-    return (
-        <>
-            <h1 className = "title">REACTIVE WEATHER</h1>
-            <h3 className = "subtitle">Up to the minute weather news</h3>
-            <div className = "app">
-                {/* Render components here */}
-                
-
-
-            </div>
-            
-        </>
-    )
+  return (
+    <>
+      <h1 className="title">REACTIVE WEATHER</h1>
+      <h3 className="subtitle">Up to the minute weather news</h3>
+      <div className="app">
+        {cities.map((city, index) => (
+          <WeatherCard key={index} data={city} />
+        ))}
+      </div>
+    </>
+  );
 }
 
 export default App;
